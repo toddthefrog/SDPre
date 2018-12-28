27e3a6fa0efb10dc40a158609798cf3d5bcfde5a
toddthefrog <todd.m.trowbridge@gmail.com>
2018-12-27 22:18:29 -0500

    Objective 1 Labs 1-3 complete

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,6 @@
+public class Objective1Lab2{
+  public static void main( String[] args ) {
+    System.out.println( "Todd Trowbridge" );
+    System.out.print( "35" );
+  }
+}
---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3{
+    public static void main(String[] args) {
+      System.out.println("Name : Todd Trowbridge");
+      System.out.println("Age : 35");
+      System.out.print("State : Georgia");
+    }
+}
