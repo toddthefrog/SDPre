4cdbdaa3decea00e2b6b3a8530207590f0cbc154
toddthefrog <todd.m.trowbridge@gmail.com>
2018-12-28 23:30:59 -0500

    Objective 2 Labs 1-3 complete

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2{
+  public static void main(String[] args){
+    String name = "Todd Trowbridge";
+    int age = 35;
+    String state = "Georgia";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+    
+  }
+}
