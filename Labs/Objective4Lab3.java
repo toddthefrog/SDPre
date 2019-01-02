a75c9e65bb647bdcd87af96d0b2fc03ed7e2a27d
toddthefrog <todd.m.trowbridge@gmail.com>
2019-01-01 22:01:11 -0500

    Objective4Lab3 complete

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Objective4Lab3{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    int userNum;
+
+    System.out.println("Please enter a number: ");
+    userNum = scanner.nextInt();
+
+    if(userNum < 0){
+      System.out.println("The number is negative");
+    }
+    else {
+      if (userNum == 0){
+        System.out.println("The number is equal to 0");
+      }
+      else {
+        System.out.println("The number is positive");
+      }
+    }
+
+    scanner.close();
+  }
+}
