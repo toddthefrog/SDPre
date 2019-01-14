8ef4ba71023e9a6268092e7e875a6e1e6c0bbdbd
Todd Trowbridge <toddtrowbridge@Brucebs-MacBook-Pro.local>
2019-01-13 20:37:07 -0700

    Objective6Lab4 complete

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,12 @@
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+    int counter = 1;
+    int sum = 0;
+
+    while(counter <= 20){
+      sum = sum + counter;
+      counter++;
+    }
+    System.out.println(sum);
+  }
+}
